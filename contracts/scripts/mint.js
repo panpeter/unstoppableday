625e3be944c80414f1cab6a9a3ada7be78ff359e
Piotr Wilczek <piotrwilczek@gmail.com>
2022-04-07 17:49:53 +0300

    Add mint script

---
+++ contracts/scripts/mint.js
@@ -0,0 +1,24 @@
+const { getContractAt } = require("@nomiclabs/hardhat-ethers/internal/helpers");
+
+const CONTRACT_ADDRESS = ""
+const NFT_ID = 0
+
+async function main() {
+    const [account] = await ethers.getSigners()
+    const dayNFTContract = await getContractAt(hre, "DayNFT", CONTRACT_ADDRESS, account)
+    await dayNFTContract.setMintActive(true, {
+        gasLimit: 500_000,
+    })
+    const transactionResponse = await dayNFTContract.mint(NFT_ID, {
+        gasLimit: 500_000,
+    })
+
+    console.log(transactionResponse)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
