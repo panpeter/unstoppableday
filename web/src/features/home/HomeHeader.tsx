23cb10cb905e6bf16cf8b8b27d92a6db968a62f7
Piotr Wilczek <piotrwilczek@gmail.com>
2022-04-26 23:07:37 +0300

    Remove unnecessary fragment

--- web/src/features/home/HomeHeader.tsx
+++ web/src/features/home/HomeHeader.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 export function HomeHeader() {
     return (
-        <React.Fragment>
-            <h1>Unstoppable Day</h1>
-        </React.Fragment>
+        <h1>Unstoppable Day</h1>
     );
 }
